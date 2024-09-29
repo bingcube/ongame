95ae7920ca8d2d04d533f9e7db32cb8e61bfa4b1
Cube <178304360+bingcube@users.noreply.github.com>
2024-09-29 16:32:30 +0700

    Update game.js

--- game.js
+++ game.js
@@ -23,6 +23,7 @@ window.addEventListener('resize', () => {
 });
 
 let player;
+let isDragging = false;
 
 function preload() {
     this.load.image('player', 'https://raw.githubusercontent.com/bingcube/source/refs/heads/main/US_Thompson.png'); // Asset súng
@@ -31,14 +32,33 @@ function preload() {
 
 function create() {
     player = this.physics.add.sprite(config.width / 2, config.height / 2, 'player').setOrigin(0.5, 0.5);
-    this.input.on('pointerdown', shootBullet, this);
+    this.input.on('pointerdown', () => { isDragging = true; });
+    this.input.on('pointerup', () => { isDragging = false; });
 }
 
 function update() {
-    // Logic to move player if needed
+    if (isDragging) {
+        player.x = this.input.x;
+        player.y = this.input.y;
+    }
+    
+    // Di chuyển bằng phím
+    const speed = 5; // Tốc độ di chuyển
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.UP)) {
+        player.y -= speed;
+    }
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.DOWN)) {
+        player.y += speed;
+    }
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
+        player.x -= speed;
+    }
+    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)) {
+        player.x += speed;
+    }
 }
 
 function shootBullet(pointer) {
-    const bullet = this.physics.add.sprite(player.x, player.y, 'bullet').setScale(0.07); // Điều chỉnh kích thước viên đạn
+    const bullet = this.physics.add.sprite(player.x, player.y, 'bullet').setScale(0.07);
     this.physics.moveTo(bullet, pointer.x, pointer.y, 600);
 }
